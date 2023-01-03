89073b8580b7dda11af69119080387959aa8b1d9
Angel <angel42505@outlook.com>
2023-01-03 08:41:30 +0200

    Updated the testimonials section - made the columns be 3 and added 2 more testimonials

--- src/sections/TestimonialsSection/TestimonialsSection.tsx
+++ src/sections/TestimonialsSection/TestimonialsSection.tsx
@@ -33,6 +33,20 @@ const initialTestimonialsContent: TestimonialType[] = [
     posted: '3 minutes ago',
     text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae expedita deserunt eligendi fugiat natus non beatae voluptatem itaque distinctio odio nulla numquam totam, eos molestias temporibus facere similique? Dolorum officiis neque placeat veritatis ducimus quod ipsam obcaecati aliquid hic nulla! Voluptatibus amet rem exercitationem ducimus! Quo nisi iure impedit quos.',
   },
+
+  {
+    name: 'John D.',
+    image: testimonialImage,
+    posted: '3 minutes ago',
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae expedita deserunt eligendi fugiat natus non beatae voluptatem itaque distinctio odio nulla numquam totam, eos molestias temporibus facere similique? Dolorum officiis neque placeat veritatis ducimus quod ipsam obcaecati aliquid hic nulla! Voluptatibus amet rem exercitationem ducimus! Quo nisi iure impedit quos.',
+  },
+
+  {
+    name: 'John D.',
+    image: testimonialImage,
+    posted: '3 minutes ago',
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae expedita deserunt eligendi fugiat natus non beatae voluptatem itaque distinctio odio nulla numquam totam, eos molestias temporibus facere similique? Dolorum officiis neque placeat veritatis ducimus quod ipsam obcaecati aliquid hic nulla! Voluptatibus amet rem exercitationem ducimus! Quo nisi iure impedit quos.',
+  },
 ];
 
 const TestimonialsSection = () => {
